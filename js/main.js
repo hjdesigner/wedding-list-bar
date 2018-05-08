8f4c40313b1dc73642a29f71baf01f6aa60b449c
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-08 13:55:08 -0300

    Finished the CSS of the details part of the list Created the function that opens and closes this part of details Placed the library to run the custom scroll

--- js/main.js
+++ js/main.js
@@ -16,9 +16,44 @@ function carousel() {
   	autoplaySpeed: 2000,
 	});
 }
+function scrollBar() {
+	var scrollDiv = document.querySelector('.weddingListBar__details-body');
+	jQuery(scrollDiv).mCustomScrollbar();
+}
+function openDetails() {
+	var buttonOpen = document.querySelector('[data-id="openDetails"]');
+	var buttonClose = document.querySelector('[data-id="detailsClose"]');
+	var text = document.querySelector('[data-id="openDetailsText"]');
+	var details = document.querySelector('[data-id="details"]');
+	buttonOpen.addEventListener('click', function() {
+		if (this.classList.contains('active')) {
+			this.classList.remove('active');
+			text.innerHTML = 'ver';
+			details.classList.remove('active');
+			return
+		}
+		details.classList.add('active');
+		text.innerHTML = 'ocultar';
+		this.classList.add('active');
+	})
+	buttonClose.addEventListener('click', function() {
+		if (buttonOpen.classList.contains('active')) {
+			buttonOpen.classList.remove('active');
+			text.innerHTML = 'ver';
+			details.classList.remove('active');
+			return
+		}
+		details.classList.add('active');
+		text.innerHTML = 'ocultar';
+		buttonOpen.classList.add('active');
+	})
+}
+
 
 window.addEventListener('DOMContentLoaded', function() {
 	OpenClose();
 	carousel();
+	scrollBar();
+	openDetails();
 })
 
